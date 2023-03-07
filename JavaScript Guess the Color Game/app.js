e4c3a10b83af7e273862684d5fc57d8e82f275ba
akhijannat <ajjannat02@gmail.com>
2023-03-07 18:31:59 +0600

    gussing game new color btn, play again btn, easy btn, hard btn complete

--- JavaScript Guess the Color Game/app.js
+++ JavaScript Guess the Color Game/app.js
@@ -5,16 +5,48 @@ let hardBtn = document.getElementById("hard-btn");
 let newColorBtn = document.getElementById("new-color");
 let playAgainBtn = document.getElementById("play-again");
 
-let boxLength = 6;
+let boxLength = 3;
 
-// Create Dynamic color
-let randomColor = Math.floor(Math.random() * 16777215).toString(16);
-
-let createBox = `<div class="bg-[#${randomColor}] h-48 w-48 cursor-pointer rounded-3xl text-center">#${randomColor}</div>`;
-
-let randomBox = Math.floor(Math.random() * boxLength);
-console.log(randomBox);
-
-for (let i = 0; i < boxLength; i++) {
-  box.innerHTML += createBox;
+// Create Dynamic color and Box
+function dynamicBoxes() {
+  for (let i = 0; i < boxLength; i++) {
+    let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    let createBox = `<div class="bg-[#${randomColor}] h-48 w-48 cursor-pointer rounded-3xl text-center"></div>`;
+    box.innerHTML += createBox;
+  }
 }
+
+// Easy Button
+easyBtn.addEventListener("click", () => {
+  hardBtn.classList.remove("bg-green-800");
+  easyBtn.classList.add("bg-green-800");
+  hardBtn.classList.remove("text-white");
+  easyBtn.classList.add("text-white");
+  box.innerHTML = "";
+  boxLength = 3;
+  dynamicBoxes();
+});
+
+// Hard Button
+hardBtn.addEventListener("click", () => {
+  easyBtn.classList.remove("bg-green-800");
+  hardBtn.classList.add("bg-green-800");
+  easyBtn.classList.remove("text-white");
+  hardBtn.classList.add("text-white");
+  box.innerHTML = "";
+  boxLength = 6;
+  dynamicBoxes();
+});
+
+// New Color
+newColorBtn.addEventListener("click", () => {
+  box.innerHTML = "";
+  dynamicBoxes();
+});
+
+// Play Again
+playAgainBtn.addEventListener("click", () => {
+  location.reload();
+});
+
+window.onload(dynamicBoxes());
