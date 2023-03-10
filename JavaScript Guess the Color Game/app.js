29f8104615323334746a49aafd0cae0ed443e899
akhijannat <ajjannat02@gmail.com>
2023-03-10 18:04:22 +0600

    updated code readability

--- JavaScript Guess the Color Game/app.js
+++ JavaScript Guess the Color Game/app.js
@@ -12,61 +12,11 @@ let boxLength = 3;
 function createBox(randomColor) {
   boxes.innerHTML +=
     /*html*/
-    `<div onclick="selectedBox(this)" data-rgb-color="${randomColor}" class="bg-[${randomColor}] border-2 h-48 w-48 cursor-pointer rounded-3xl text-center">
-      ${randomColor}
-    </div>`;
-}
-
-// Create Dynamic color
-let arry = [];
-dynamicBoxes();
-function dynamicBoxes(correctColor = null) {
-  arry.length = 0; // Reset array previous value
-  for (let i = 0; i < boxLength; i++) {
-    let randomColor;
-    if (correctColor) {
-      randomColor = correctColor;
-    } else {
-      randomColor = getRandomRGBColor();
-    }
-    arry.push(randomColor);
-    createBox(randomColor);
-  }
-}
-let randomArryItem;
-function arryItem() {
-  randomArryItem = arry[Math.floor(Math.random() * arry.length)];
-}
-
-arryItem();
-
-guessingColor();
-function guessingColor() {
-  // Show background color dynamically
-  let bgColor =
-    /*html*/
-    `<div class="text-white text-4xl uppercase text-center p-8">
-      <h1>THE GREAT</h1>
-      <h1 class="text-6xl py-3 font-bold">${randomArryItem}</h1>
-      <h1>GUESSING GAME</h1>
-    </div>`;
-
-  headerBgColor.innerHTML = bgColor;
-}
-
-function selectedBox(e) {
-  if (randomArryItem === e.dataset.rgbColor) {
-    headerBgColor.classList.remove("bg-gray-500");
-    headerBgColor.classList.add(`bg-[${randomArryItem}]`);
-    message.innerHTML = `Correct`;
-    newColorBtn.classList.add("hidden");
-    playAgainBtn.classList.remove("hidden");
-    boxes.innerHTML = "";
-    dynamicBoxes(randomArryItem);
-  } else {
-    e.classList.add("opacity-0");
-    message.innerHTML = `Try Again`;
-  }
+    `<div 
+      onclick="checkingGuess(this)" 
+      data-rgb-color="${randomColor}" 
+      class="bg-[${randomColor}] border-2 h-48 w-48 cursor-pointer rounded-3xl text-center"
+    ></div>`;
 }
 
 // Create Random color
@@ -77,17 +27,96 @@ function getRandomRGBColor() {
   return "rgb(" + r + "," + g + "," + b + ")";
 }
 
+// Create Dynamic color Boxes
+let colorsArray = []; // define array to store all colors
+dynamicBoxes(); //call this to generate default color boxes
+function dynamicBoxes(correctColor = null) {
+  colorsArray.length = 0; // Reset array previous value
+
+  for (let i = 0; i < boxLength; i++) {
+    let randomColor;
+
+    if (correctColor) {
+      randomColor = correctColor;
+    } else {
+      randomColor = getRandomRGBColor();
+    }
+
+    colorsArray.push(randomColor); //adding color to colors Array
+    createBox(randomColor); // generating color boxes on demand
+  }
+}
+
+/**
+ * selecting random color item from color boxes
+ * to match user guessing color
+ */
+let randomColorFromColorsArray;
+getARandomColorFromColorsArray();
+function getARandomColorFromColorsArray() {
+  randomColorFromColorsArray =
+    colorsArray[Math.floor(Math.random() * colorsArray.length)];
+}
+
+/**
+ * rendering top header section
+ * showing selected random color code in RGB
+ */
+headerSection();
+function headerSection() {
+  headerBgColor.innerHTML =
+    /*html*/
+    `<div class="text-white text-4xl uppercase text-center p-8">
+      <h1>THE GREAT</h1>
+      <h1 class="text-6xl py-3 font-bold">${randomColorFromColorsArray}</h1>
+      <h1>GUESSING GAME</h1>
+    </div>`;
+}
+
+/**
+ * checking users guess
+ * render feedback
+ * @param {this or current div} element
+ */
+function checkingGuess(element) {
+  /**
+   * matching user's guess with the selected random color
+   */
+  if (randomColorFromColorsArray === element.dataset.rgbColor) {
+    // changing background color in header section
+    headerBgColor.classList.remove("bg-gray-500");
+    headerBgColor.classList.add(`bg-[${randomColorFromColorsArray}]`);
+
+    // updating related buttons and message
+    message.innerHTML = `Correct`;
+    newColorBtn.classList.add("hidden");
+    playAgainBtn.classList.remove("hidden");
+
+    // updating color of boxes
+    boxes.innerHTML = "";
+    dynamicBoxes(randomColorFromColorsArray);
+  } else {
+    /**
+     * if user's guess wrong
+     * hide that box
+     * updating message
+     */
+    element.classList.add("opacity-0");
+    message.innerHTML = `Try Again`;
+  }
+}
+
 // Easy Button
 easyBtn.addEventListener("click", () => {
   hardBtn.classList.remove("bg-green-800");
   easyBtn.classList.add("bg-green-800");
   hardBtn.classList.remove("text-white");
   easyBtn.classList.add("text-white");
-  boxes.innerHTML = "";
-  boxLength = 3;
-  dynamicBoxes();
-  arryItem();
-  guessingColor();
+  boxes.innerHTML = ""; // reset color boxes
+  boxLength = 3; // color boxes amount
+  dynamicBoxes(); // rendering boxes
+  getARandomColorFromColorsArray(); // selecting random color item from color boxes
+  headerSection(); // updating header section
 });
 
 // Hard Button
@@ -96,19 +125,19 @@ hardBtn.addEventListener("click", () => {
   hardBtn.classList.add("bg-green-800");
   easyBtn.classList.remove("text-white");
   hardBtn.classList.add("text-white");
-  boxes.innerHTML = "";
-  boxLength = 6;
-  dynamicBoxes();
-  arryItem();
-  guessingColor();
+  boxes.innerHTML = ""; // reset color boxes
+  boxLength = 6; // color boxes amount
+  dynamicBoxes(); // rendering boxes
+  getARandomColorFromColorsArray(); // selecting random color item from color boxes
+  headerSection(); // updating header section
 });
 
 // New Color
 newColorBtn.addEventListener("click", () => {
-  boxes.innerHTML = "";
-  dynamicBoxes();
-  arryItem();
-  guessingColor();
+  boxes.innerHTML = ""; // reset color boxes
+  dynamicBoxes(); // rendering boxes
+  getARandomColorFromColorsArray(); // selecting random color item from color boxes
+  headerSection(); // updating header section
 });
 
 // Play Again
