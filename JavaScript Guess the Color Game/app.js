2da498adf759476130ad5f632a27573d11689b53
akhijannat <ajjannat02@gmail.com>
2023-03-10 13:44:05 +0600

    javascript color guessing game - massege,header bg update

--- JavaScript Guess the Color Game/app.js
+++ JavaScript Guess the Color Game/app.js
@@ -1,19 +1,72 @@
-let rgb = document.getElementById("rgb");
+let headerBgColor = document.getElementById("header-bg");
 let box = document.getElementById("box");
 let easyBtn = document.getElementById("easy-btn");
 let hardBtn = document.getElementById("hard-btn");
 let newColorBtn = document.getElementById("new-color");
 let playAgainBtn = document.getElementById("play-again");
+let message = document.getElementById("message");
 
 let boxLength = 3;
 
-// Create Dynamic color and Box
+// Create Dynamic Box
+function createBox(randomColor) {
+  box.innerHTML +=
+    /*html*/
+    `<div onclick="selectedBox(this)" data-rgb-color="${randomColor}" class="bg-[${randomColor}] border-2 h-48 w-48 cursor-pointer rounded-3xl text-center">
+      ${randomColor}
+    </div>`;
+}
+// Create Dynamic color
+let arry = [];
+dynamicBoxes();
 function dynamicBoxes() {
+  arry.length = 0; // Reset array previous value
   for (let i = 0; i < boxLength; i++) {
-    let randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    let createBox = `<div class="bg-[#${randomColor}] h-48 w-48 cursor-pointer rounded-3xl text-center"></div>`;
-    box.innerHTML += createBox;
+    let randomColor = getRandomRGBColor();
+    arry.push(randomColor);
+    createBox(randomColor);
   }
+}
+let randomArryItem;
+function arryItem() {
+  randomArryItem = arry[Math.floor(Math.random() * arry.length)];
+}
+
+arryItem();
+
+guessingColor();
+function guessingColor() {
+  // Show background color dynamically
+  let bgColor =
+    /*html*/
+    `<div class="text-white text-4xl uppercase text-center p-8">
+      <h1>THE GREAT</h1>
+      <h1 class="text-6xl py-3 font-bold">${randomArryItem}</h1>
+      <h1>GUESSING GAME</h1>
+    </div>`;
+
+  headerBgColor.innerHTML = bgColor;
+}
+
+function selectedBox(e) {
+  if (randomArryItem === e.dataset.rgbColor) {
+    headerBgColor.classList.remove("bg-gray-500");
+    headerBgColor.classList.add(`bg-[${randomArryItem}]`);
+    message.innerHTML = `Correct`;
+    newColorBtn.classList.add("hidden");
+    playAgainBtn.classList.remove("hidden");
+  } else {
+    e.classList.add("opacity-0");
+    message.innerHTML = `Try Again`;
+  }
+}
+
+// Create Random color
+function getRandomRGBColor() {
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+  return "rgb(" + r + "," + g + "," + b + ")";
 }
 
 // Easy Button
@@ -25,6 +78,8 @@ easyBtn.addEventListener("click", () => {
   box.innerHTML = "";
   boxLength = 3;
   dynamicBoxes();
+  arryItem();
+  guessingColor();
 });
 
 // Hard Button
@@ -36,17 +91,19 @@ hardBtn.addEventListener("click", () => {
   box.innerHTML = "";
   boxLength = 6;
   dynamicBoxes();
+  arryItem();
+  guessingColor();
 });
 
 // New Color
 newColorBtn.addEventListener("click", () => {
   box.innerHTML = "";
   dynamicBoxes();
+  arryItem();
+  guessingColor();
 });
 
 // Play Again
 playAgainBtn.addEventListener("click", () => {
   location.reload();
 });
-
-window.onload(dynamicBoxes());
