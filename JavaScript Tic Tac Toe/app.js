c0392a62441d934901868e6ea73d6bb85ae17e5e
akhijannat <ajjannat02@gmail.com>
2023-03-15 13:54:03 +0600

    show selected symbol in box

--- JavaScript Tic Tac Toe/app.js
+++ JavaScript Tic Tac Toe/app.js
@@ -1,12 +1,31 @@
-let box = document.querySelectorAll(".box");
+let restart = document.getElementById("restart");
+let gameBox = document.getElementsByClassName("game-box");
 
-let cross = "x";
-let zero = "0";
-let count = 9;
+let selectedSymbol = "X";
 
-let symbol = Math.floor(Math.random() * count);
-
-for (let i = 0; i < box; i++) {
-  box.innerHTML = cross;
+for (let i = 0; i < gameBox.length; i++) {
+  gameBox[i].addEventListener("click", () => {
+    if (selectedSymbol === "X") {
+      selectedSymbol = "0";
+    } else {
+      selectedSymbol = "X";
+    }
+    gameBox[i].innerHTML = selectedSymbol;
+  });
 }
-console.log(symbol);
+
+let wining = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7],
+];
+
+// Restart Button
+restart.addEventListener("click", () => {
+  location.reload();
+});
