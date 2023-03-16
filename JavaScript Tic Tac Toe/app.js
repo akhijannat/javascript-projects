352bee63355e50cef3296c5446e8f1ae9de748da
akhijannat <ajjannat02@gmail.com>
2023-03-16 11:36:29 +0600

    get selected symbol in different array

--- JavaScript Tic Tac Toe/app.js
+++ JavaScript Tic Tac Toe/app.js
@@ -3,13 +3,19 @@ let gameBox = document.getElementsByClassName("game-box");
 
 let selectedSymbol = "X";
 
+let personOne = [];
+let personTwo = [];
+
 for (let i = 0; i < gameBox.length; i++) {
   gameBox[i].addEventListener("click", () => {
     if (selectedSymbol === "X") {
       selectedSymbol = "0";
+      personOne.push(gameBox[i].dataset.number);
     } else {
       selectedSymbol = "X";
+      personTwo.push(gameBox[i].dataset.number);
     }
+
     gameBox[i].innerHTML = selectedSymbol;
   });
 }
@@ -24,6 +30,13 @@ let wining = [
   [1, 5, 9],
   [3, 5, 7],
 ];
+
+// checkWin();
+// function checkWin() {
+//   for (let i = 0; i < wining.length; i++) {
+//     console.log(wining[i]);
+//   }
+// }
 
 // Restart Button
 restart.addEventListener("click", () => {
